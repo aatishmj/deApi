178d7a6faa95e10eaa271956e6af1f972308cd36
aatishmj <aatishjawalkar13@gmail.com>
2023-09-18 16:32:41 +0530

    modified:   de_app/views.py

--- de_app/views.py
+++ de_app/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
 from .models import *
-from .serializers import *
 from django.core.exceptions import ObjectDoesNotExist
 from .serializers import emp_Serializer , emp_work_Serializer ,Ad_data_Serializer
 # Create your views here.
@@ -13,9 +12,9 @@ class EmployeeViewSet(viewsets.ModelViewSet):
 
 
 class EmployeeworkViewSet(viewsets.ModelViewSet):
-    queryset = emp.objects.all()
+    queryset = emp_work.objects.all()
     serializer_class = emp_work_Serializer
 
 class adminViewSet(viewsets.ModelViewSet):
-    queryset = emp.objects.all()
+    queryset = Ad_data.objects.all()
     serializer_class = Ad_data_Serializer
