47bde24d39960cc597b5f1437333071987840891
aatishmj <aatishjawalkar13@gmail.com>
2023-09-18 16:55:54 +0530

    modified:   db.sqlite3 	modified:   de_app/__pycache__/models.cpython-310.pyc 	modified:   de_app/__pycache__/views.cpython-310.pyc 	new file:   de_app/migrations/0002_alter_emp_id.py 	new file:   de_app/migrations/__pycache__/0002_alter_emp_id.cpython-310.pyc 	modified:   de_app/models.py

--- de_app/models.py
+++ de_app/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 # Create your models here.
 class emp(models.Model) :
-    id=models.CharField(max_length=10,primary_key=True)
     name=models.TextField(max_length=50)
     phone_no=models.IntegerField()
     email=models.EmailField(blank=True)
